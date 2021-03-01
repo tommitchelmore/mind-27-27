6290a3fc99438288a0070f014694c2a689493de4
Thomas Mitchelmore <tommitchelmore@outlook.com>
2021-03-01 20:04:41 +0000

    Add 404 case to justGiving ID

--- pages/api/justGiving/[id].js
+++ pages/api/justGiving/[id].js
@@ -1,10 +1,15 @@
 import axios from "axios"
 
 export default async (req, res) => {
-    const { data: {fundraisingTarget, totalRaisedOnline : totalRaised } } = await axios.get(`https://api.justgiving.com/${process.env.JUSTGIVING_APP_ID}/v1/fundraising/pages/${req.query.id}`)
+    try {
+        const { data: {fundraisingTarget, totalRaisedOnline : totalRaised } } = await axios.get(`https://api.justgiving.com/${process.env.JUSTGIVING_APP_ID}/v1/fundraising/pages/${req.query.id}`)
     
-    return res.json({
-        fundraisingTarget,
-        totalRaised
-    });
+        return res.json({
+            fundraisingTarget,
+            totalRaised
+        });
+    }
+    catch(e) {
+        return res.status(404).json({"error": "Not found"})
+    }
 }
